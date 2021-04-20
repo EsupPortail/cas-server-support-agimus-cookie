471ab0f3822e0cd80d2db0f1cf34c9d6523ba393
Vincent Bonamy <vbonamy@gmail.com>
2021-04-20 10:26:07 +0200

    Fix AgimusCookieAction

--- src/main/java/org/esupportail/cas/flow/AgimusCookieAction.java
+++ src/main/java/org/esupportail/cas/flow/AgimusCookieAction.java
@@ -79,6 +79,6 @@ public class AgimusCookieAction extends AbstractNonInteractiveCredentialsAction 
         } catch (final Exception e) {
             LOGGER.warn(e.getMessage(), e);
         }
-        return null;
+        return WebUtils.getCredential(requestContext);
     }
 }
